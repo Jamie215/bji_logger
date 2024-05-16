2c957dbf627eed11582d5228fd24228b4c51ca85
jamie215 <jmkim0603@gmail.com>
2024-05-16 10:25:44 -0400

    Added setup.py for cx_freeze build

---
+++ setup.py
@@ -0,0 +1,26 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but some might need fine-tuning
+build_exe_options = {
+    "include_files": ["assets/"],
+    "build_exe": "BJI_Logger"
+}
+
+# Base can be "Win32GUI" if you're building a GUI application on Windows
+base = None
+
+executables = [
+    Executable(
+        "index.py",
+        base=base,
+        icon="BJI_Logger_icon.ico"
+    )
+]
+
+setup(
+    name="BJI_Logger",
+    version="2.4",
+    description="BJI CORK Project Data Application",
+    options={"build_exe": build_exe_options},
+    executables=executables
+)
